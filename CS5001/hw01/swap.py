10f3731e7ec47a0b130fdba59b393ea2d33aed55
vincentchan <everest115@outlook.com>
2018-09-10 22:33:07 -0700

    Homework1

---
+++ CS5001/hw01/swap.py
@@ -0,0 +1,17 @@
+# Get input.
+word1 = input("Enter word #1: ")
+word2 = input("Enter word #2: ")
+
+# Enter your code here to swap what’s 
+# in the variables word1 and word2.
+
+temp = word1
+word1 = word2
+word2 = temp
+
+# Print results.
+print () # prints blank line
+
+print("Swapping results")
+print("word #1:", word1)
+print("word #2:", word2)
